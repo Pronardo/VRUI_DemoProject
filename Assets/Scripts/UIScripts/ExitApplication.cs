9457c9f6e447952b239cd20ef9991b16edbf7f6b
Pronardo <projetekt@outlook.de>
2018-07-23 16:20:07 +0200

    Created Menu Template
    Added Components for Menu Designing

---
+++ Assets/Scripts/UIScripts/ExitApplication.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitApplication : MonoBehaviour
+{
+    //For those who wanna go
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
