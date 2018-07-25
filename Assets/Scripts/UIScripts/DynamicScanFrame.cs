456db813eb4e2b75dd646a8635ea7b9e2a9aea6d
Pronardo <projetekt@outlook.de>
2018-07-25 10:48:28 +0200

    Implemented DynamicScanFrame
    Implemented DynamicScanFrame to allow changing ScanFrame size during runt-time as additional option

---
+++ Assets/Scripts/UIScripts/DynamicScanFrame.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ScanFrameResizer))]
+public class DynamicScanFrame : MonoBehaviour
+{
+    //DynamicScanFrame is optional and allows to change resp. adapt the size of the ScanFrame during run-time
+    //DynamicScanFrame uses ScanFrameResizer's methods to operate
+    private ScanFrameResizer resizer;
+    private GameObject scannable;
+    private Vector3 currentRegistedScales;
+
+    void Start()
+    {
+        resizer = this.GetComponent<ScanFrameResizer>();
+        if (resizer == null) { Debug.LogError("ScanFrameResizer is needed as base script for DynamicScanFrame"); this.enabled = false; }
+        scannable = getCurrentScannable();
+        currentRegistedScales = resizer.GetScannableSize();
+    }
+
+    void Update()
+    {
+        if (currentRegistedScales != resizer.GetScannableSize())
+        {
+            resizer.ResizeFrame();
+        }
+    }
+
+    public GameObject getCurrentScannable()
+    {
+        scannable = resizer.ScannableObject; if (scannable == null) { Debug.Log("scannable is null!"); }
+        return scannable;
+    }
+}
--- Assets/Scripts/UIScripts/ScanFrameResizer.cs
+++ Assets/Scripts/UIScripts/ScanFrameResizer.cs
@@ -13,23 +13,33 @@ public class ScanFrameResizer : MonoBehaviour {
     [Tooltip("The relation between the depths of the the ScannableObject and the ScanningFrame(Input: 0.5 means 50% of the ScannableObject's depth")]
     public float RelativeFrameDepth = 0.5f;
    
-    void Awake() {
-        if (ScannableObject == null)
-        {
-            Debug.Log("object reference null!");
-        }
-    
-        Renderer scannableRenderer = ScannableObject.GetComponent<Renderer>();
-        if (scannableRenderer == null)
-        {
-            Debug.Log("scannableRenderer reference null!");
-        }
-        Vector3 scannableDimension = scannableRenderer.bounds.size;
+    void OnEnable() {
+        ResizeFrame(); Debug.Log("ScanFrameResizer: ScanFrameResizer enabled - call ResizeFrame()");
+    }
+    public void ResizeFrame()
+    {
+        Vector3 scannableDimension = GetScannableSize();
         float RelativeFrameWidthX = scannableDimension.x * FramePerScaleX;
         float RelativeFrameWidthY = scannableDimension.y * FramePerScaleY;
         this.transform.localScale =
-        new Vector3(scannableDimension.x + RelativeFrameWidthX, scannableDimension.y + RelativeFrameWidthY, scannableDimension.z*RelativeFrameDepth);
+        new Vector3(scannableDimension.x + RelativeFrameWidthX, scannableDimension.y + RelativeFrameWidthY, scannableDimension.z * RelativeFrameDepth);
         /*Vector3 parentPos = this.transform.parent.transform.position;
         this.transform.position = new Vector3(parentPos.x+parentDimension.x/2,parentPos.y,parentPos.z);*/
+    }
+    public Vector3 GetScannableSize()
+    {
+        if (ScannableObject == null)
+        {
+            Debug.LogError("ScanFrameResizer: ScannableObject reference null!");
+            this.enabled = false;
+        }
+
+        Renderer scannableRenderer = ScannableObject.GetComponent<Renderer>();
+        if (scannableRenderer == null)
+        {
+            Debug.LogError("ScanFrameResizer: Invalid ScannableObject - no renderer found!");
+            this.enabled = false;
+        }
+        return scannableRenderer.bounds.size;
     }
 }
