04f4d0646af2c2e49ccd8139ed9fa692dbc378b4
Pronardo <projetekt@outlook.de>
2018-07-25 13:11:54 +0200

    Intermediate Commit
    A commit for safety not meaning

---
+++ Assets/Scripts/UIScripts/DisplayMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayMenu : MonoBehaviour {
+
+    private GameObject menu;
+
+	void Start () {
+        menu=GameObject.Find("Menu");
+        if(menu==null)
+        {
+            menu = Instantiate((GameObject)Resources.Load("Prefabs/Menu"));
+        }
+        HideMenu();
+	}
+	
+    public void ShowMenu()
+    {
+        Debug.Log("DisplayMenu: ShowMenu()");
+        menu.SetActive(true);
+    }
+
+    public void HideMenu()
+    {
+        Debug.Log("DisplayMenu: HideMenu()");
+        menu.SetActive(false);
+    }
+}
