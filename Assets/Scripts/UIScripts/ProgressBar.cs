cdb094ab4706bc5e8cb5a7abd25736f7897edab7
Pronardo <projetekt@outlook.de>
2018-07-30 16:44:04 +0200

    Intermediate Commit
    A commit for safety not meaning

---
+++ Assets/Scripts/UIScripts/ProgressBar.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBar : MonoBehaviour
+{
+    public Transform LoadingBar;
+    public Transform TextIndicator;
+    [Tooltip("The delay [in milliseconds] for hiding the loading in progress elements")]
+    public int HideDelay = 500;
+
+    private float fillTime;
+    private VREyeRaycaster raycaster;
+    private float currentAmount;
+    private float percentage;
+    private Image progressGraph;
+
+    private VRInteractiveItem[] interactables;
+    private TimeGazeable[] gazeables;
+
+    private void OnEnable()
+    {
+        Debug.Log("ProgressBar - OnEnable");
+        var cam = GameObject.Find("VRCamera");
+        if (cam == null) { Debug.LogError("VR without VRCamera is horribly wrong!"); this.enabled = false; }
+        raycaster = cam.GetComponent<VREyeRaycaster>();
+        progressGraph = this.transform.Find("ProgressGraph").GetComponent<Image>();
+        fillTime = raycaster.GazeTimer;
+        gazeables = GetComponents<TimeGazeable>();
+        foreach (var gazeable in gazeables)
+        {
+            gazeable.InsideGaze += RefreshLoadingState;
+            gazeable.OutOfGaze += Reset;
+        }
+    }
+
+    /*void Update () {
+	if(currentAmount<fillTime)
+        {
+            currentAmount += Time.deltaTime;
+            percentage = currentAmount / fillTime;
+            TextIndicator.GetComponent<Text>().text = ((int)(percentage*100)).ToString() + "%";
+        }
+        else
+        {
+            Debug.Log("ProgressLoading - ProgressBarFull");
+            Wait(HideDelay);
+            TextIndicator.gameObject.SetActive(false);
+            LoadingBar.gameObject.SetActive(false);
+            this.enabled = false;
+        }
+        LoadingBar.GetComponent<Image>().fillAmount = percentage;
+    }*/
+
+    public void RefreshLoadingState(float amount)
+    {
+        Debug.Log("ProgressBar - RefreshLoadingState");
+        progressGraph.fillAmount=amount/fillTime; Debug.Log("" + amount / fillTime);
+        if(TextIndicator!=null)
+        {
+            TextIndicator.GetComponent<Text>().text = ((int)(percentage * 100)).ToString() + "%";
+        }
+    }
+
+    private void Wait(int milliseconds)
+    {
+        System.Threading.Thread.Sleep(milliseconds);
+    }
+
+    public void Reset()
+    {
+        Debug.Log("ProgressBar - Reset");
+        progressGraph.fillAmount = 0;
+        TextIndicator.GetComponent<Text>().text = "";
+    }
+
+    private void OnDisable()
+    {
+        foreach (var gazeable in gazeables)
+        {
+            gazeable.InsideGaze -= RefreshLoadingState;
+            gazeable.OutOfGaze -= Reset;
+        }
+    }
+}
--- Assets/Scripts/UIScripts/TimeGazeable.cs
+++ Assets/Scripts/UIScripts/TimeGazeable.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,20 +8,43 @@ public class TimeGazeable : MonoBehaviour
     private float timeRemaining;
     private float gazeTimer;
 
+    public delegate void GazeInsideEvent(float amount);
+    public GazeInsideEvent InsideGaze;
+    public delegate void GazeOutEvent();
+    public GazeOutEvent OutOfGaze;
+
+
     private void OnEnable()
     {
-        gazeTimer = GameObject.Find("VRCamera").GetComponent<VREyeRaycaster>().GazeTimer;
-        timeRemaining = gazeTimer;
+        Debug.Log("TimeGazeable - OnEnable");
+        var cam = GameObject.Find("VRCamera").transform;
+        if (cam == null) { Debug.LogError("VR without VRCamera is horribly wrong!"); this.enabled = false; }
+        gazeTimer = cam.GetComponent<VREyeRaycaster>().GazeTimer;
+        timeRemaining = gazeTimer;        
     }
 
     void Update()
     {
         timeRemaining -= Time.deltaTime;
-        if (timeRemaining <= 0)
+        if (InsideGaze != null)
         {
-            this.GetComponent<ItemEventHandler>().OnActivation();
+            Debug.Log("TimeGazeable - AddTime to ProgressBar");
+            InsideGaze(Time.deltaTime);
+        }
+        if (timeRemaining < 0)
+        {
             Debug.Log("TimeGazeable: Sharp Gaze - Do Action!");
+            this.GetComponent<ItemEventHandler>().OnActivation();
+
             this.enabled = false;
+        }
+    }
+    private void OnDisable()
+    {
+        Debug.Log("TimeGazeable - OnDisable");
+        if (OutOfGaze != null)
+        {
+            OutOfGaze();
         }
     }
 }
